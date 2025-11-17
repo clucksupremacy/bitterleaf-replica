e5a7e13cf02e086794ad04044dd9cc408c4fcabe
clucksupremacy <cherrycluckcluck@gmail.com>
2025-11-16 22:46:00 -0800

    supabase-netlify version

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,17 +1,41 @@
-import ProductCard from './ProductCard';
-import { useEffect, useState } from "react";
+// // for localhost postgres connection
+// import ProductCard from './ProductCard';
+// import { useEffect, useState } from "react";
 
+// export default function Gallery() {
+//   const [products, setProducts] = useState([]);
+
+//   useEffect(() => {
+//     fetch("/api/products")
+//       .then(res => res.json())
+//       .then(data => setProducts(data));
+//   }, []);
+
+//   return (
+//     <div className="container-gallery">
+//       {products.map(product => (
+//         <ProductCard key={product.id} product={product} />
+//       ))}
+//     </div>
+//   );
+// }
+
+
+// for supabase-netlify postgres connection
+import ProductCard from './ProductCard';
+import { supabase } from '../../lib/supabaseClient';
+import { useEffect, useState } from "react";
 
 export default function Gallery() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("/api/products")
-      .then(res => res.json())
-      .then(data => setProducts(data));
+    supabase
+      .from('products')
+      .select('*')
+      .then(({ data }) => setProducts(data));
   }, []);
 
-  
   return (
     <div className="container-gallery">
       {products.map(product => (
--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -1,16 +1,26 @@
-import pool from "../../../lib/db";
+// // for localhost postgres connection
+// import pool from "../../../lib/db";
 
-export default async function handler(req, res) {
-    if (req.method !== "GET") {
-        return res.status(405).json({ error: "Method not allowed" });
-    }
+// export default async function handler(req, res) {
+//     if (req.method !== "GET") {
+//         return res.status(405).json({ error: "Method not allowed" });
+//     }
 
-    try {
-        const result = await pool.query("SELECT * FROM products ORDER BY id DESC");
+//     try {
+//         const result = await pool.query("SELECT * FROM products ORDER BY id DESC");
 
-        res.status(200).json(result.rows);
-    } catch (err) {
-        console.error("DB ERROR:", err);
-        res.status(500).json({ error: "Database error" });
-    }
+//         res.status(200).json(result.rows);
+//     } catch (err) {
+//         console.error("DB ERROR:", err);
+//         res.status(500).json({ error: "Database error" });
+//     }
+// }
+
+// for supabase-netlify postgres connection
+import { supabase } from '../../../lib/supabaseClient';
+
+export async function GET() {
+    const { data, error } = await supabase.from('products').select('*');
+    if (error) return new Response(error.message, { status: 500 });
+    return new Response(JSON.stringify(data), { status: 200 });
 }
